4b569109c7be88e8b7aba7a989f3764240a0bd0b
Tair Sangama <tairsangam@gmail.com>
2025-08-12 03:07:13 -0500

    Actualizar estilos, estructura HTML Y script; agregar carpeta de imágenes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Para una navegación suave al hacer click en los enlaces
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
